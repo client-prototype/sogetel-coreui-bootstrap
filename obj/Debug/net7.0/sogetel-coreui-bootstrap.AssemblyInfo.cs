fd6bc423e6a1a26621917fa68f808c5bf0769d42
client-prototype <153096259+client-prototype@users.noreply.github.com>
2023-12-26 15:43:17 -0500

    d
    d

--- obj/Debug/net7.0/sogetel-coreui-bootstrap.AssemblyInfo.cs
+++ obj/Debug/net7.0/sogetel-coreui-bootstrap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sogetel-coreui-bootstrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b616b2c56f93e278b7f522b75d2f4a588a7f4cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("sogetel-coreui-bootstrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sogetel-coreui-bootstrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
